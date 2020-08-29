a00ed8ddaba0d6bb54ad89b93ef239e9ee6ddb47
GOKULKRISHNANGS <krishnansadasivam96@gmail.com>
2020-08-29 07:52:21 +0530

    Added Prettier

--- helloworld/routes/hello.js
+++ helloworld/routes/hello.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send("Hello World");
+router.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
 module.exports = router;
